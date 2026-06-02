b65c4a13c717fa9184f733d1289c6501e8290bae
sassdahrak123 <sassdahrak123@gmail.com>
2026-06-03 03:18:23 +0700

    Correction previous week

--- Struct/01_basic.cpp
+++ Struct/01_basic.cpp
@@ -10,46 +10,75 @@ struct Person{
 
 // 3 1.default constructor, 2.primary constructor 3.secondary constructor.
 
-
-/**
+/** Memory Block
  * 1. 3
  * 2. 5343434343
  * ...
  * 100.
  */
-/**
- * // default constructor
+/** default constructor
  * accountName = "" 
  * type = ""
  * amount = 56453454 gabage
  * accountId = 23434343
  */
 struct BankAccount{
-    string accountName;
-    string type;
-    int amount;
-    int accountId;
+    // default value
+    string account_name = "";
+    string type = "";
+    int amount = 0;  // gabage value
+    int accountId = 0;
 
-    BankAccount(string name, string type, int amount, int id) : accountName(name), type(type), amount(amount), accountId(id) {
+    /**
+     * default constructor should have only one inside struct or class
+     */
+    // default constructor
+    BankAccount() = default;
+
+    // defalult constructor
+    BankAccount(): account_name(""), type(""), amount(0), accountId(0) {}
+
+    // primary constructor
+    BankAccount(string name,
+         string type, 
+         int amount, 
+         int id) : 
+         account_name(name), 
+         type(type),
+         amount(amount), 
+         accountId(id) {
         // cout << "name" << name << endl;
         // cout << i << endl;
     }
 
-    // cout << name << endl;
-    // cout << accountName << endl;
+};
+/**
+ * type convention 
+ *    - structure PascalCase
+ *    - class PascalCase
+ *    - function camelCase
+ *    - variable snake_case
+ *    - instance | obj camelCase
+ *  
+ *  default constructor:
+ *    - build in: it auto have when their is no manual constructor. but it break if we create one constructor inside struct.
+ *    - use default keyward 
+ *    - init value with out pass parameter 
+ */
+struct Product{
+    float price; // 10 byte - stack 1010 0000 101020...
+    int num; // 4 byte - stack 
+    string type; // heap - "hi I am virak" 1010 1010 1010 1010 1010 1010 ... (13 byte)
+    string brand; // 
 
-    // for(int i = 0; i < 10; i++){
-
-    // }
-    // cout << i << endl;
-
+    Product() = default;
 };
 
-struct Drink{
 
+struct Drink{
     int quantiy;
     float price;
-
+    // BankAccount(): account_name(""), type(""), amount(0), accountId(0) {}
 };
 
 /**
@@ -64,9 +93,13 @@ int my_first_function(int x){
 }
 int main(){
     
+    int user_name; //keep 4 byte memory to my program -> gabage value       
+    user_name = 123;
+
     // Drink mathcha;
     // Drink coca;
-    
+    // int choice;
+
     int my_num;
     Person p1;
     p1.name = "virak";
@@ -84,11 +117,16 @@ int main(){
     cout << "age: " << p1.age << endl;
     cout << "contact: " << p1.contact << endl;
 
-    BankAccount ac("my Account", "Saving account", 1000, 113527);
+    // Research how to use or access default constructor
+    BankAccount ac;
+    ac.account_name = "labubu";
 
     int my_value = my_first_function(1); //3
 
-    BankAccount ABA("my Account", "Saving account", 1000, 113527);
-    int my_num;
+    BankAccount aba("my Account", "Saving account", 1000, 113527);
+    // int my_num;
+
+    Product veaJa;
+
     return 0;
 }
